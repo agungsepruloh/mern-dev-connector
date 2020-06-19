ad71cb0df8dcbd72efb87c38ae89b291d452f166
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-06-19 07:34:55 +0700

    Implement JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
@@ -58,8 +60,22 @@ router.post(
 			await user.save();
 
 			// Return jsonwebtoken
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
 
-			res.send("User registered");
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"),
+				// TODO: change the expires time to 3600 when deployment
+				{ expiresIn: 36000 },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
 		} catch (err) {
 			console.log(err.message);
 			res.status(500).send("Server error");
