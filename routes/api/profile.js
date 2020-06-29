99f2721dda2155574b266e5dd64cc361269b291d
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-06-29 07:39:07 +0700

    Delete account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult, body } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const Post = require("../../models/Post");
 const { json, response } = require("express");
 
 // @route   GET api/profile/me
@@ -154,8 +155,8 @@ router.get("/user/:user_id", async (req, res) => {
 // @access  Private
 router.delete("/", auth, async (req, res) => {
 	try {
-		// TODO: Remove posts
-
+		// Remove user posts
+		await Post.deleteMany({ user: req.user.id });
 		// Remove profile
 		await Profile.findOneAndRemove({ user: req.user.id });
 		// Remove user
