17547ec733783c464a622aa40250adff1458cbb1
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-06-29 07:03:32 +0700

    List education & experiences

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,7 +4,9 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import DashboardActions from "./DashboardActions";
+import Experience from "./Experience";
 import { getCurrentProfile } from "../../actions/profile";
+import Education from "./Education";
 
 const Dashboard = ({
 	getCurrentProfile,
@@ -25,6 +27,8 @@ const Dashboard = ({
 			{profile !== null ? (
 				<Fragment>
 					<DashboardActions />
+					<Experience experience={profile.experience} />
+					<Education education={profile.education} />
 				</Fragment>
 			) : (
 				<Fragment>
