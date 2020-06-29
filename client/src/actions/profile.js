9ba79a7d6af995196ff61d7907099f325c2b304d
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-06-30 06:34:01 +0700

    Start on the profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -56,7 +56,7 @@ export const getProfiles = () => async (dispatch) => {
 // Get Profile by ID
 export const getProfileById = (userId) => async (dispatch) => {
 	try {
-		const res = await axios.get(`/api/profile/user/${userId}}`);
+		const res = await axios.get(`/api/profile/user/${userId}`);
 
 		dispatch({
 			type: GET_PROFILE,
